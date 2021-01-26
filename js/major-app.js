612f36c821b67efae89bb8506bc5a9a401db38db
bahazghayar <bahazghayar@hotmail.com>
2021-01-26 19:47:42 +0200

    icons, chart, and layout edited

--- js/major-app.js
+++ js/major-app.js
@@ -70,4 +70,43 @@ function userClick8(event) {
     event.preventDefault();
     video.removeAttribute('src', 'https://www.youtube.com/embed/MSjPW4ZMtus');
     video.setAttribute('src', 'https://www.youtube.com/embed/sgGb8BM2TBk');
-} 
+}
+
+
+let mychart = document.getElementById('mychart').getContext('2d');
+let barchart = new Chart(mychart, {
+    type: 'pie',
+    data: {
+        labels: ['Surgeons / Doctors', 'Judges', 'Lawyers', 'Bank Managers', 'Chief Executive Officers', 'Chief Financial Officers', 'Orthodontists', 'College Professors', 'Pilots', 'Marketing Directors', 'Software Developers'],
+        datasets: [{
+            label: 'JOD',
+            data: [ 7000, 4500, 4000, 3700, 3500, 2600, 2200, 2000, 1750, 1500 ,1000],
+            backgroundColor: [
+                'rgba(183, 20, 0, 0.5)',
+                'rgba(183, 142, 0, 0.5)',
+                'rgba(183, 235, 0, 0.5)',
+                'rgba(183, 235, 183, 0.5)',
+                'rgba(183, 235, 3, 1)',
+                'rgba(255, 235, 3, 1)',
+                'rgba(255, 88, 3, 1)',
+                'rgba(255, 88, 102, 1)',
+                'rgba(255, 88, 203, 1)',
+                'rgba(255, 25, 34, 0.2)',
+                'rgba(255, 27, 35, 0.3)'
+
+            ],
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Top AVG Paid Jobs in Jordan',
+            fontSize: 25
+        },
+        legend: {
+            position: 'right',
+        }
+        
+    }
+}
+);
